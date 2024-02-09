586494196e8c914d11ee9d733019de028b077698
kayanbrandao <kayanbrandao.krb@gmail.com>
2024-02-08 22:54:57 -0300

    init: creating the car store project.

---
+++ src/main/java/com/storecarwithspring/StoreCarWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.storecarwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreCarWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreCarWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/storecarwithspring/StoreCarWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storecarwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreCarWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
